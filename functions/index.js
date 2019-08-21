964e588b3ec6f07ceb659771678bceff4a7f5daa
Éderson Tiago Szlachta <edersonszlachta@gmail.com>
2019-08-21 10:53:12 -0300

    Auto cleanup old messages

--- functions/index.js
+++ functions/index.js
@@ -7,18 +7,22 @@ const config     = require('./config/app.js');
 
 admin.initializeApp();
 
-const box   = admin.database().ref('/INBOX');
+
+const inbox = admin.database().ref('/INBOX');
 const index = admin.database().ref('/INDEX');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ * Host data into Firebase Realtime Database
+ **/
 app.post(`/${config.endpoint}`, function(request, response, next) {
 
 	let user_hash = crypto.createHash('md5').update(request.body['recipient'] || 'default').digest('hex');
 	let now = Date.now();
-	let message = box.child(user_hash).push({
+	let message = inbox.child(user_hash).push({
 		from       : request.body['from']       || null,
 		recipient  : request.body['recipient']  || null,
 		sender     : request.body['sender']     || null,
@@ -30,12 +34,38 @@ app.post(`/${config.endpoint}`, function(request, response, next) {
 	});
 
 	index.push({
-      box        : user_hash,
-      message    : message.key,
-      created_at : now
-    })
+		box        : user_hash,
+		message    : message.key,
+		created_at : now
+	})
 
-	response.send({"message":"Post received. Thanks!"});
+	response.send({"message":`Received and hosted at INBOX/${user_hash}/${message.key}`});
+
+	removeOldMessages();
 });
+
+
+/**
+ * Remove old messages
+ **/
+const CUT_OFF_TIME = Date.now() - 4 * 86400000;
+async function removeOldMessages() {
+  const items = await index.orderByChild('created_at').endAt(CUT_OFF_TIME).once('value');
+
+  items.forEach(child => {
+    let item = child.val();
+
+    inbox.child(item.box).child(item.message).remove().then(function() {
+       index.child(child.key).remove().then(function() {
+        return console.log('[%s] removed /INBOX/%s/%s  and  /INDEX/%s   thoose were created at %s', (new Date()).toISOString(), item.box, item.message, child.key, ((new Date(item.created_at)).toISOString()));
+      }).catch(function(error) {
+        return console.error('[%s] failed /INDEX/%s with error: %s', (new Date()).toISOString(), child.key, error.message);
+      });
+    }).catch(function(error) {
+      return console.error('[%s] failed /INBOX/%s/%s with error: %s', (new Date()).toISOString(), item.box, item.message, error.message);
+    });
+  });
+}
+
 
 exports.mailgun = functions.https.onRequest(app);
